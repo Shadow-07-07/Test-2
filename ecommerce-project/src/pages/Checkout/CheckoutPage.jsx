497aa649fbd132299af959aa9fd94b303e4b60f9
chandra <s.chandrasaikrishna6254@gmail.com>
2026-07-06 19:03:22 +0530

    make a separate useEffect for  payment summary

--- ecommerce-project/src/pages/Checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/Checkout/CheckoutPage.jsx
@@ -11,15 +11,20 @@ export function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
-      setDeliveryOptions(response.data);
-
-      response = await axios.get('/api/payment-summary');
-      setPaymentSummary(response.data);
+      const response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+      setDeliveryOptions(response.data); 
     }
 
     fetchCheckoutData();
-  }, [cart]);
+  }, []);
+  useEffect(() => {
+    const fetchPaymentData = async () => {
+      const response = await axios.get('/api/payment-summary');
+      setPaymentSummary(response.data);
+    }
+
+    fetchPaymentData();
+  },[cart]);
 
   return (
     <>
